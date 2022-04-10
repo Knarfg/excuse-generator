6d3b338979b9b3de7fe55e7183ac64a617d61677
Knarfg <frankgonzalez033@gmail.com>
2022-04-10 20:43:06 +0000

    Cambios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
+
+let who = ["El perro", "El gato"];
+let action = ["se comió", "rompió"];
+let what = ["la tarea", "los papeles"];
+let when = ["ayer", "la semana pasada"];
+
+function selectRandom(arr) {
+  let random = ~~(Math.random() * arr.length);
+  return arr[random];
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let result = `${selectRandom(who)} ${selectRandom(action)} 
+  ${selectRandom(what)} ${selectRandom(when)}`;
+  let element = document.getElementById("excuse");
+  element.innerHTML = result;
 };
